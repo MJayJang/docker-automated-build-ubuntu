8373847018c3ca2f63619aebc3e581f26fa513fb
MJayJang <mjismy@gmail.com>
2019-10-12 23:32:54 +0900

    Modified port value of Node server.js

--- temp-project/server.js
+++ temp-project/server.js
@@ -1,15 +1,15 @@
+'use strict';
+
 const express = require('express')
 
-const PORT = 5227;
+const PORT = 8080;
 const HOST = '0.0.0.0';
 
-var app = express();
-
-app.use(function (request, response) {
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    response.end('<h1>Hello express</h1>');
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello world\n');
 });
 
-app.listen(PORT, function(){
-    console.log(`Server Running on http://${HOST}:${PORT}`);
-})
+app.listen(PORT, HOST);
+console.log(`Server Running on http://${HOST}:${PORT}`);
